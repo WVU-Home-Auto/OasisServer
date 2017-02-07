6e1cb74a4c21ac279b87b37924f54d31264ffcac
ryanrosier <rmrosier@mix.wvu.edu>
2017-02-07 13:31:55 -0500

    I think this is what is going on for EventListeners, but i could be wrong

--- oasisserver/src/main/java/edu/wvu/solar/oasisserver/plugins/EventListeners.java
+++ oasisserver/src/main/java/edu/wvu/solar/oasisserver/plugins/EventListeners.java
@@ -15,11 +15,23 @@ import org.mapdb.Serializer;
 import javax.print.attribute.standard.MediaSize;
 
 public class EventListeners {
-
-    private static final Logger LOGGER = LogManager.getLogger(EventListeners.class);
-    
-    public void addRecipe(){
-    	//TODO: do this
+	
+	private static final Logger LOGGER = LogManager.getLogger(EventListeners.class);
+	
+	private String eventType;
+	private List<ParameterComparison> comparisons;
+	private List<Parameter> parameters;
+	private DeviceManager deviceManager;
+	
+	public EventListeners(String eventType, List<ParameterComparison> comparisons, List<Parameter> parameters, DeviceManager deviceManager){
+		this.eventType = eventType;
+		this.comparisons = comparisons;
+		this.parameters = parameters;
+		this.deviceManager = deviceManager;
+	}
+	
+    public void addRecipe(String eventType, List<ParameterComparison> comparisons, List<Parameter> parameters, DeviceManager deviceManager){
+    	Recipe(eventType, comparisons, parameters, deviceManager);
     }
     
     public void eventTriggered(Event event){
