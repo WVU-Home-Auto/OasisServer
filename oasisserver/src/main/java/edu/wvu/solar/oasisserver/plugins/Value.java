3b08844f82e7d7d190fbef7e7d229122bf2e5cde
ryanrosier <rmrosier@mix.wvu.edu>
2017-04-03 17:09:41 -0400

    finished value

--- oasisserver/src/main/java/edu/wvu/solar/oasisserver/plugins/Value.java
+++ oasisserver/src/main/java/edu/wvu/solar/oasisserver/plugins/Value.java
@@ -18,6 +18,10 @@ public class Value implements Comparable<Value>{
 	public Object getValue(){
 	    return this.value;
     }
+	
+	public String getName(){
+		return name;
+	}
 
 	public int compareTo(Value other) {
 	    switch(type){
@@ -25,7 +29,23 @@ public class Value implements Comparable<Value>{
                 Integer otherInt = (Integer) other.getValue();
                 Integer myInt = (Integer) this.value;
                 return myInt.compareTo(otherInt);
-            // Etc.
+            case BOOLEAN:
+            	Boolean otherBool = (Boolean) other.getValue();
+            	Boolean myBool = (Boolean) this.value;
+            	return myBool.compareTo(otherBool);
+            case STRING:
+            	String otherStr = (String) other.getValue();
+            	String myStr = (String) this.value;
+            	return myStr.compareTo(otherStr);
+            case FLOAT:
+            	Float otherDbl = (Float) other.getValue();
+            	Float myDbl = (Float) this.value;
+            	return myDbl.compareTo(otherDbl);
+            case DEVICE:
+            	String otherDevice = (String) other.getValue();
+            	String myDevice = (String) this.value;
+            	return myDevice.compareTo(otherDevice);
+                // Etc.
         }
 	    return -1;
 	}
